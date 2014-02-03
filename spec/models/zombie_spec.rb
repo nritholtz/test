5054483e3e516db74dbf4c78521ff76d3d3356b0
nritholtz <nritholtz@gmail.com>
2014-02-03 18:04:42 -0500

    Done with Rspec for day

--- spec/models/zombie_spec.rb
+++ spec/models/zombie_spec.rb
@@ -1,27 +1,26 @@
 	require 'spec_helper'
 	describe Zombie do
+		let(:ash) {Zombie.new(name: "Ash Clone 1")}
 		it "is invalid without a name" do
 			should_not be_valid
 		end
 
 		it "has a name that matches 'Ash Clone'" do
-			zombie = Zombie.new(name: "Ash Clone 1")
-			expect(zombie.name).to match(/Ash Clone \d/)
+			expect(ash.name).to match(/Ash Clone \d/)
 		end
 
 		it 'includes tweets' do
 			tweet1 = Tweet.new(message: 'test')
 			tweet2 = Tweet.new(message: 'test')
-			zombie= Zombie.new(name:'Ash', tweets:[tweet1,tweet2])
-			expect(zombie.tweets).to include(tweet1)
-			expect(zombie.tweets).to include(tweet2)
+			ash.tweets = [tweet1, tweet2]
+			expect(ash.tweets).to include(tweet1)
+			expect(ash.tweets).to include(tweet2)
 		end	
 
 			its('tweets.size'){should == 0}
 
-		it "changes the number of Zombies" do
-			zombie = FactoryGirl.build(:zombie)
-			expect {zombie.save}.to change {Zombie.count}.by(1)
+		it "when created changes the number of Zombies" do
+			expect {ash.save}.to change {Zombie.count}.by(1)
 		end
 
 		it "raises an error if saved without a name" do
@@ -29,4 +28,10 @@
 		end
 
 		it {should respond_to(:name)}
+
+		context "who is rotting" do
+			subject{Zombie.new(rotting: true)}
+
+			its(:rotting){should == true}
+		end
 	end
